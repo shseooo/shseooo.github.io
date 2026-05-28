1766f973d16aa552e327379c50bb6469293a9768
shseooo <shseo@massivelinks.co.jp>
2026-05-28 15:12:21 +0900

    delete

--- .astro/content-modules.mjs
+++
@@ -1,6 +0,0 @@
-
-export default new Map([
-["src/content/blog/en/welcome.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fen%2Fwelcome.mdx&astroContentModuleFlag=true")],
-["src/content/blog/ja/welcome.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fja%2Fwelcome.mdx&astroContentModuleFlag=true")],
-["src/content/blog/ko/welcome.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fko%2Fwelcome.mdx&astroContentModuleFlag=true")]]);
-		
